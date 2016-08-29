0320bca7184966fa10b804da64a00fcc456aaa37
RodrigoCastilloOlfos <rodrigocastilloolfos@gmail.com>
2016-08-29 18:34:25 -0300

    deberia

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,17 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  #para mostrar contenido a registrado
+  before_filter :authenticate_user!, except:[ :index, :show]
+
+  def pinsof
+  @user_id = params[:user_id]
+  @user = User.find(@user_id)
+  @pins = @user.pins
+  end
+
+  def mypins
+    @pins = current_user.pins
+  end
 
   # GET /pins
   # GET /pins.json
@@ -10,12 +22,12 @@ class PinsController < ApplicationController
   # GET /pins/1
   # GET /pins/1.json
   def show
-      @pin = Pin.find params[:id] #-> or Pin.first desdeelarrobanocorrespondealcurso
+    @pin = Pin.find params[:id] #-> or Pin.first desdeelarrobanocorrespondealcurso
   end
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+     @pin = current_user.pins.build
   end
 
   # GET /pins/1/edit
@@ -25,32 +37,24 @@ class PinsController < ApplicationController
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
+      @pin = current_user.pins.build(pin_params)
 
-    respond_to do |format|
       if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
+          redirect_to @pin, notice: "Successfully created new Pin"
       else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+          render 'new'
       end
-    end
   end
 
   # PATCH/PUT /pins/1
   # PATCH/PUT /pins/1.json
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pin }
-      else
-        format.html { render :edit }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+          if @pin.update(pin_params)
+              redirect_to @pin, notice: "Pin was Successfully updated!"
+          else
+              render 'edit'
+          end
       end
-    end
-  end
 
   # DELETE /pins/1
   # DELETE /pins/1.json
@@ -70,6 +74,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:photo, :description)
+      params.require(:pin).permit(:photo, :description, :name)
     end
 end
